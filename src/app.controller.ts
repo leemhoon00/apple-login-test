94c274b1baca3cddc045120bcbc314a7dfa89f2d
leemhoon00 <leemhoon000@gmail.com>
2025-07-08 16:59:40 +0900

    dddddd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AppleLoginDto } from './apple.dto';
+import { jwtDecode } from 'jwt-decode';
 
 @Controller()
 export class AppController {
   @Post('/apple-login')
   appleLogin(@Body() body: AppleLoginDto) {
-    console.log('Received body:', body);
+    const { code, id_token } = body;
+    const decodedToken = jwtDecode(id_token);
+
+    console.log('code:', code);
+
+    console.log('decodedToken:', decodedToken);
   }
 }
