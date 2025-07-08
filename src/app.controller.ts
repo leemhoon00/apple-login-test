5d7270fa0b434767ad4037436af01b156f0344cf
leemhoon00 <leemhoon000@gmail.com>
2025-07-08 17:03:34 +0900

    dd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,8 +9,24 @@ export class AppController {
     const { code, id_token } = body;
     const decodedToken = jwtDecode(id_token);
 
-    console.log('code:', code);
+    // console.log('code:', code);
+    // console.log('decodedToken:', decodedToken);
 
-    console.log('decodedToken:', decodedToken);
+    const publicKeys = getApplePublicKey();
+    console.log(publicKeys);
   }
+}
+
+async function getApplePublicKey() {
+  const response = await fetch('https://appleid.apple.com/auth/keys', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unnecessary-type-assertion
+  const data = (await response.json()) as any;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  return data.keys as string[];
 }
