db29e9701e59c5e70f057fa1bd246cc1523ee52b
leemhoon00 <leemhoon000@gmail.com>
2025-07-08 16:48:14 +0900

    asdfsd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { AppleLoginDto } from './apple.dto';
 
 @Controller()
 export class AppController {
   @Post('/apple-login')
-  appleLogin(@Body() body: { code: string; id_token: string }) {
+  appleLogin(@Body() body: AppleLoginDto) {
     console.log('Received body:', body);
   }
 }
