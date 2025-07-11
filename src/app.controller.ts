b3cd5191ddbac1ee0baf11273f99328b76797765
leemhoon00 <leemhoon000@gmail.com>
2025-07-11 17:26:44 +0900

    d

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,38 +1,21 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { AppleLoginDto } from './apple.dto';
-import { jwtDecode } from 'jwt-decode';
+import { Controller, Get, Res, Req } from '@nestjs/common';
+import { Response, Request } from 'express';
 
 @Controller()
 export class AppController {
-  @Post('/apple-login')
-  async appleLogin(@Body() body: AppleLoginDto) {
-    const { code, id_token } = body;
-    const decodedHeaders = jwtDecode(id_token, { header: true });
-    const decodedToken = jwtDecode(id_token);
-
-    console.log('code:', code);
-    console.log('decodedToken:', decodedToken);
-    console.log('decodedHeaders:', decodedHeaders);
-
-    const publicKeys = await getApplePublicKey();
-    console.log(publicKeys);
-
-    return {
-      message: 'Apple login successful',
-    };
+  @Get('set')
+  setCookie(@Res() res: Response) {
+    res.cookie('myCookie', 'cookieValue', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+    });
   }
-}
 
-async function getApplePublicKey() {
-  const response = await fetch('https://appleid.apple.com/auth/keys', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unnecessary-type-assertion
-  const data = (await response.json()) as any;
+  @Get('get')
+  getCookie(@Req() req: Request) {
+    const cookieValue = req.cookies as string[];
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  return data.keys as string[];
+    console.log(cookieValue);
+  }
 }
